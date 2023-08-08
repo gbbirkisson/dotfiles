7c4c960b5d37fab3e613d70691fec1e283764dfa
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-08-08 12:39:26 +0200

    Use nvim as git merge tool

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -16,4 +16,15 @@ M.abc = {
   }
 }
 
+-- For vim diff mode
+if vim.opt.diff:get() then
+  local d = {
+    ["dn"] = {"]c", "Vim [D]iff [N]ext"},
+    ["dp"] = {"[c", "Vim [D]iff [P]revious"},
+    ["dl"] = {"<cmd> :diffget LOCAL <CR>", "Vim [D]iff Use [L]ocal"},
+    ["dr"] = {"<cmd> :diffget REMOTE <CR>", "Vim [D]iff Use [R]remote"},
+  }
+  for k,v in pairs(d) do M.abc.n[k] = v end
+end
+
 return M
