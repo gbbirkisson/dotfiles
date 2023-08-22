483b9e3157d808bc23d73b74c0ab488ed54cf43e
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-08-22 08:54:44 +0200

    Fix river files

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,2 +1,9 @@
 -- Common mappings with Intellij
 vim.cmd("source ~/.config/nvim/lua/custom/common.vim")
+
+-- Use terraform treesitter parser for river files
+vim.filetype.add({
+  extension = {
+    river = 'terraform'
+  },
+})
