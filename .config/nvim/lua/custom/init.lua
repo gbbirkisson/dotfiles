c45cd1aba770b130d094e2b9f99de8e569da8a24
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-11-01 12:43:56 +0100

    Add vim Gl command and set default

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,2 +1,16 @@
 -- Common mappings with Intellij
 vim.cmd("source ~/.config/nvim/lua/custom/common.vim")
+
+-- Add function to set colorcolumn and textwidth
+vim.api.nvim_create_user_command(
+  'Gl',
+  function(opts)
+    vim.api.nvim_set_option_value("colorcolumn", opts.fargs[1], {})
+    vim.api.nvim_set_option_value("textwidth", tonumber(opts.fargs[1]), {})
+  end,
+  { nargs = 1 }
+)
+
+-- Default to 100 (rust standard)
+-- Note: format selection with gq
+vim.cmd.Gl("100")
