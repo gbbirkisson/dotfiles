5e8ce32d44f1137a4a54ef06a0b1753c41331c42
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-11-07 20:21:10 +0100

    Use gpt-4

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -132,6 +132,12 @@ local plugins = {
     config = function()
       require("chatgpt").setup({
         api_key_cmd = "op read op://y46glakwqtek75fxiejq6is3qa/qnpgz2gbdyqdcp4cnrobbzttfm/key --no-newline",
+        openai_params = {
+         model = "gpt-4",
+        },
+        openai_edit_params = {
+         model = "gpt-4",
+        },
       })
     end,
   },
