41c371b4ff3850a0850b47760aa5b8f09428f024
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-12-18 14:44:33 +0100

    Update custom direnv poetry function

--- .config/direnv/lib/poetry.sh
+++ .config/direnv/lib/poetry.sh
@@ -1,22 +1,24 @@
 #!/bin/sh
 
-layout_poetry() {
+POETRY_BIN=$(which poetry)
+
+poetry() {
 	PYPROJECT_TOML="${PYPROJECT_TOML:-pyproject.toml}"
 	if [ ! -f "$PYPROJECT_TOML" ]; then
 		log_status "No pyproject.toml found. Executing \`poetry init\` to create a \`$PYPROJECT_TOML\` first."
-		poetry init
+		$POETRY_BIN init -q
 	fi
 
 	if [ -d ".venv" ]; then
 		VIRTUAL_ENV="$(pwd)/.venv"
 	else
-		VIRTUAL_ENV="$(poetry env list --full-path | head -1 | awk '{print $1}')"
+		VIRTUAL_ENV="$($POETRY_BIN env list --full-path | head -1 | awk '{print $1}')"
 	fi
 
 	if [ -z "$VIRTUAL_ENV" ] || [ ! -d "$VIRTUAL_ENV" ]; then
 		log_status "No virtual environment exists. Executing \`poetry install\` to create one."
-		poetry install
-		VIRTUAL_ENV=$(poetry env info --path)
+		$POETRY_BIN install
+		VIRTUAL_ENV="$($POETRY_BIN env list --full-path | head -1 | awk '{print $1}')"
 	fi
 
 	PATH_add "$VIRTUAL_ENV/bin"
