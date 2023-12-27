ca59b25e19a582016c8b883e89857b1635d3b72c
Guðmundur Björn Birkisson <gbbirkisson@gmail.com>
2023-12-27 21:14:24 +0100

    Add vim zen mode

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -160,7 +160,44 @@ local plugins = {
         },
       })
     end,
-  }
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = { "ZenMode" },
+    opts = {
+      window = {
+        backdrop = 0.98,
+        width = 96,
+      },
+      plugins = {
+        options = {
+          enabled = true,
+          ruler = false, -- disables the ruler text in the cmd line area
+          showcmd = false, -- disables the command in the last line of the screen
+          laststatus = 0, -- turn off the statusline in zen mode
+        },
+        gitsigns = { enabled = true }, -- disables git signs
+        tmux = { enabled = true }, -- disables the tmux statusline
+      },
+      on_open = function(win)
+        -- Disable line numbers
+        vim.cmd("set nu!")
+
+        -- Set line width
+        vim.cmd("Gl 96")
+
+        -- Disable autocomplete
+        require('cmp').setup.buffer { enabled = false }
+      end,
+      on_close = function()
+        -- Enable line numbers
+        vim.cmd("set nu")
+
+        -- Enable autocomplete
+        require('cmp').setup.buffer { enabled = true }
+      end,
+    }
+  },
 }
 
 return plugins
