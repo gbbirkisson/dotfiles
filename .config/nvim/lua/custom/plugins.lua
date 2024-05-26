6e6177a3f5e4c88e11df87a496f49c314e35bb6c
Guðmundur Björn Birkisson <gbb@gbb.is>
2024-05-26 22:13:08 +0200

    feat(nvim): Add flash plugin

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -38,19 +38,34 @@ local plugins = {
       'xiyaowong/telescope-emoji.nvim',
     },
   },
-  -- {
-  --   -- Leap with s/S
-  --   'ggandor/leap.nvim',
-  --   keys = { 's', 'S' },
-  --   config = function()
-  --     local leap = require 'leap'
-  --     leap.add_default_mappings()
-  --
-  --     -- Disable auto jump
-  --     leap.opts.safe_labels = {}
-  --     leap.opts.labels = 'sfnjklhodweimbuyvrgtaqpcxzæSFNJKLHODWEIMBUYVRGTAQPCXZÆ'
-  --   end,
-  -- },
+  {
+    -- Like leap, only better
+    'folke/flash.nvim',
+    version = 'v1.18.3',
+    event = 'VeryLazy',
+    opts = {
+      labels = 'asdfghjklqwertyuiopzxcvbnmæð',
+      label = {
+        uppercase = false,
+      },
+      highlight = {
+        backdrop = false,
+      },
+      modes = {
+        char = {
+          highlight = { backdrop = false },
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
   {
     -- Tool installation
     'williamboman/mason.nvim',
