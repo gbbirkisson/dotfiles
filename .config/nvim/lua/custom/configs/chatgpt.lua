c4fdc09c5badc89b1822779241fa9ceda04c2992
Guðmundur Björn Birkisson <gbb@gbb.is>
2024-08-04 17:40:08 +0200

    fix(nvim): Configuration for ChatGPT

--- .config/nvim/lua/custom/configs/chatgpt.lua
+++ .config/nvim/lua/custom/configs/chatgpt.lua
@@ -1,17 +1,17 @@
 return {
   config = function()
     require('chatgpt').setup {
-      api_key_cmd = 'op read op://y46glakwqtek75fxiejq6is3qa/qnpgz2gbdyqdcp4cnrobbzttfm/key --no-newline',
+      api_key_cmd = 'op read op://hemwnx22rvxga5v2zkicawq6sq/l6mbqk77zt62hrdxahozqi6uom/key --no-newline',
       edit_with_instructions = {
         keymaps = {
           use_output_as_input = '<C-y>',
         },
       },
       openai_params = {
-        model = 'gpt-4',
+        model = 'gpt-4o',
       },
       openai_edit_params = {
-        model = 'gpt-4',
+        model = 'gpt-4o',
       },
     }
   end,
