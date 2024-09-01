3023e3c7619d4896c24ff5afa9bb1fd7d3289de1
Guðmundur Björn Birkisson <gbb@gbb.is>
2024-09-01 21:28:49 +0200

    fix: open nvimtree automatically

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -21,6 +21,24 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+vim.api.nvim_create_autocmd({ 'VimEnter' }, {
+  desc = 'Open NvimTree on startup',
+  callback = function(data)
+    -- buffer is a real file on the disk
+    local real_file = vim.fn.filereadable(data.file) == 1
+
+    -- buffer is a [No Name]
+    local no_name = data.file == '' and vim.bo[data.buf].buftype == ''
+
+    if not real_file and not no_name then
+      return
+    end
+
+    -- open the tree, find the file but don't focus it
+    require('nvim-tree.api').tree.toggle { focus = false, find_file = true }
+  end,
+})
+
 -- Add function to set colorcolumn and textwidth
 vim.api.nvim_create_user_command('Gl', function(opts)
   vim.api.nvim_set_option_value('colorcolumn', opts.fargs[1], {})
