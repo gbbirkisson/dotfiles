501bc744530d25b2c05cf60e4afbe4991be233d5
Guðmundur Björn Birkisson <gbb@gbb.is>
2024-09-05 18:59:40 +0200

    feat(fish): custom binding for suggestion completion

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -45,6 +45,14 @@ if status is-interactive
     function fish_user_key_bindings
         fish_default_key_bindings -M insert
         fish_vi_key_bindings --no-erase insert
+
+        # Accept whole suggestion with ctrl+l
+        bind \cl accept-autosuggestion
+        bind -M insert \cl accept-autosuggestion
+
+        # Accept next word in suggestion with ctrl+k
+        bind \ck forward-word
+        bind -M insert \ck forward-word
     end
     set fish_cursor_default block
     set fish_cursor_insert line
