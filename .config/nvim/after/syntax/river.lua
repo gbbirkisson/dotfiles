1a670a8148ce1a9a97e10ccd14394538e90befb9
Guðmundur Björn Birkisson <gbb@gbb.is>
2024-10-09 14:07:21 +0200

    fix(nvim): update ftplugin config

---
+++ .config/nvim/after/syntax/river.lua
@@ -0,0 +1,6 @@
+vim.g.sleuth_gitcommit_heuristics = 0 -- disable sleuth
+
+vim.opt.tabstop = 2 -- how many columns of whitespace is a \t char worth?
+vim.opt.shiftwidth = 2 -- how many columns of whitespace a “level of indentation” is worth?
+vim.opt.softtabstop = 2 -- how many columns of whitespace is a tab keypress or a backspace keypress worth?
+vim.opt.expandtab = true -- you never wanna see a \t again in your file — rather, tabs keypresses will be expanded into spaces
--- .config/nvim/ftplugin/sh.vim
+++
@@ -1,4 +0,0 @@
-set tabstop=4     " how many columns of whitespace is a \t char worth?
-set shiftwidth=4  " how many columns of whitespace a “level of indentation” is worth?
-set softtabstop=4 " how many columns of whitespace is a tab keypress or a backspace keypress worth?
-set expandtab     " you never wanna see a \t again in your file — rather, tabs keypresses will be expanded into spaces
