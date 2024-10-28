5764fcda5fcc0ac3f66f0d6b4853fcfda6f02541
Guðmundur Björn Birkisson <gbb@gbb.is>
2024-10-28 08:29:00 +0100

    fix(nvim): use tabs in river files

--- .config/nvim/after/syntax/river.lua
+++ .config/nvim/after/syntax/river.lua
@@ -1,6 +1,6 @@
 vim.g.sleuth_gitcommit_heuristics = 0 -- disable sleuth
 
-vim.opt.tabstop = 2 -- how many columns of whitespace is a \t char worth?
-vim.opt.shiftwidth = 2 -- how many columns of whitespace a “level of indentation” is worth?
-vim.opt.softtabstop = 2 -- how many columns of whitespace is a tab keypress or a backspace keypress worth?
-vim.opt.expandtab = true -- you never wanna see a \t again in your file — rather, tabs keypresses will be expanded into spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 0
+vim.opt.expandtab = false
