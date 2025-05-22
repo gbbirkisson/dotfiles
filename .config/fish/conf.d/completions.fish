b127d07800351986be85a986be907bb92f2cfdcd
Guðmundur Björn Birkisson <gbb@gbb.is>
2025-05-22 20:55:01 +0200

    fix: direnv hook

--- .config/fish/conf.d/completions.fish
+++ .config/fish/conf.d/completions.fish
@@ -2,16 +2,16 @@ if set -q FISH_DEBUG
     echo (status --current-filename)
 end
 
+if command -v direnv >/dev/null
+    direnv hook fish | source
+end
+
 function gen_completions --description "generate completions"
     set DIR $HOME/.config/fish/completions
 
     bash -c "rm $DIR/*"
     if command -v a2l >/dev/null
         a2l completion fish >$DIR/a2l.fish
-    end
-
-    if command -v direnv >/dev/null
-        direnv hook fish >$DIR/direnv.fish
     end
 
     if command -v warp-cli >/dev/null
