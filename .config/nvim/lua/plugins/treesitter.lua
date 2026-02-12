17fd469cdc04ede2638f9d577de420673887018c
Guðmundur Björn Birkisson <gbb@gbb.is>
2026-02-12 22:17:24 +0100

    fix: treesitter bug

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,8 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     opts = function()
-      local parser_config = require("nvim-treesitter.parsers")
-
       local ensure = {
         "bash",
         "c",
@@ -35,52 +33,78 @@ return {
         "yaml",
       }
 
-      local river_path = vim.fn.expand("~/repos/personal/tree-sitter-river")
-      if vim.fn.isdirectory(river_path) == 1 then
-        -- setup river: https://github.com/grafana/tree-sitter-river
-        parser_config.river = {
+      -- setup river: https://github.com/grafana/tree-sitter-river
+      local function register_river_parser()
+        local p = require("nvim-treesitter.parsers")
+        p.river = {
           install_info = {
-            url = "~/repos/personal/tree-sitter-river", -- local path or git repo
-            files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
-            -- optional entries:
-            branch = "main", -- default branch in case of git repo if different from master
-            generate_requires_npm = false, -- if stand-alone parser without npm dependencies
-            requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+            url = "https://github.com/grafana/tree-sitter-river",
+            branch = "main",
+            queries = "queries",
           },
-          filetype = "river", -- if filetype does not match the parser name
+          filetype = "river",
         }
-        vim.filetype.add({
-          extension = {
-            river = "river",
-            alloy = "river",
-          },
-        })
-        -- :TSInstall river
-
-        table.insert(ensure, "river")
       end
+      register_river_parser()
+      vim.api.nvim_create_autocmd("User", { pattern = "TSUpdate", callback = register_river_parser })
+      vim.filetype.add({
+        extension = {
+          river = "river",
+          alloy = "river",
+        },
+      })
+      table.insert(ensure, "river")
 
+      local function register_rhai_parser()
+        local p = require("nvim-treesitter.parsers")
+        p.rhai = {
+          install_info = {
+            url = "https://github.com/elkowar/tree-sitter-rhai",
+            branch = "main",
+            queries = "queries",
+          },
+          filetype = "rhai",
+        }
+      end
+      register_rhai_parser()
+      vim.api.nvim_create_autocmd("User", { pattern = "TSUpdate", callback = register_rhai_parser })
+      vim.filetype.add({
+        extension = {
+          rhai = "rhai",
+        },
+      })
+      table.insert(ensure, "rhai")
+
+      -- setup vcl/vtc: https://github.com/M4R7iNP/varnishls
+      -- These parsers don't ship pre-built src/parser.c, so they can't be installed
+      -- via :TSInstall. Instead, clone the repo and generate the parsers manually:
+      --   git clone https://github.com/M4R7iNP/varnishls ~/repos/personal/varnishls
+      --   cd ~/repos/personal/varnishls/vendor/tree-sitter-vcl && tree-sitter generate
+      --   cd ~/repos/personal/varnishls/vendor/tree-sitter-vtc && tree-sitter generate
+      -- Then run :TSInstall vcl and :TSInstall vtc
       local vcl_path = vim.fn.expand("~/repos/personal/varnishls")
-      if vim.fn.isdirectory(river_path) == 1 then
-        for _, lang in pairs({ "vcl", "vtc" }) do
-          parser_config[lang] = {
-            install_info = {
-              url = "~/repos/personal/varnishls/vendor/tree-sitter-" .. lang,
-              files = { "src/parser.c" },
-            },
-            filetype = lang,
-          }
+      if vim.fn.isdirectory(vcl_path) == 1 then
+        local function register_vcl_parsers()
+          local p = require("nvim-treesitter.parsers")
+          for _, lang in pairs({ "vcl", "vtc" }) do
+            p[lang] = {
+              install_info = {
+                path = vcl_path .. "/vendor/tree-sitter-" .. lang,
+              },
+              filetype = lang,
+            }
+          end
         end
-
+        register_vcl_parsers()
+        vim.api.nvim_create_autocmd("User", { pattern = "TSUpdate", callback = register_vcl_parsers })
         vim.filetype.add({
           extension = {
             vcl = "vcl",
             vtc = "vtc",
           },
         })
-        -- not installed automatically, run:
-        -- :TSInstallFromGrammar vcl
-        -- :TSInstallFromGrammar vtc
+        table.insert(ensure, "vcl")
+        table.insert(ensure, "vtc")
       end
 
       return {
