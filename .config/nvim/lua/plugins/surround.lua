f52522f0e5ada19f1b90a0e06cb30ddd40aff45a
Guðmundur Björn Birkisson <gbb@gbb.is>
2026-03-04 11:16:49 +0100

    fix: nvim surround config

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,19 +1,24 @@
 return {
   "kylechui/nvim-surround",
-  laxy = false,
-  opts = {
-    keymaps = {
-      insert = "<C-g>s",
-      insert_line = "<C-g>S",
-      normal = "yt",
-      normal_cur = "ytt",
-      normal_line = "yT",
-      normal_cur_line = "yTT",
-      visual = "T",
-      visual_line = "gT",
-      delete = "dt",
-      change = "ct",
-      change_line = "cT",
-    },
-  },
+  lazy = false,
+  init = function()
+    vim.g.nvim_surround_no_mappings = true
+  end,
+  config = function()
+    require("nvim-surround").setup()
+    -- Insert mode
+    vim.keymap.set("i", "<C-g>s", "<Plug>(nvim-surround-insert)")
+    vim.keymap.set("i", "<C-g>S", "<Plug>(nvim-surround-insert-line)")
+    -- Normal mode
+    vim.keymap.set("n", "yt", "<Plug>(nvim-surround-normal)")
+    vim.keymap.set("n", "ytt", "<Plug>(nvim-surround-normal-cur)")
+    vim.keymap.set("n", "yT", "<Plug>(nvim-surround-normal-line)")
+    vim.keymap.set("n", "yTT", "<Plug>(nvim-surround-normal-cur-line)")
+    vim.keymap.set("n", "dt", "<Plug>(nvim-surround-delete)")
+    vim.keymap.set("n", "ct", "<Plug>(nvim-surround-change)")
+    vim.keymap.set("n", "cT", "<Plug>(nvim-surround-change-line)")
+    -- Visual mode
+    vim.keymap.set("x", "T", "<Plug>(nvim-surround-visual)")
+    vim.keymap.set("x", "gT", "<Plug>(nvim-surround-visual-line)")
+  end,
 }
