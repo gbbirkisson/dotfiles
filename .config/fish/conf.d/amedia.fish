f14ab2e7bf03d483654bf2893338310a9d727f2b
Guðmundur Björn Birkisson <gbb@gbb.is>
2026-03-23 08:39:59 +0100

    fix: amedia aliases

--- .config/fish/conf.d/amedia.fish
+++ .config/fish/conf.d/amedia.fish
@@ -11,3 +11,14 @@ setenv SITECONFIG_PATH $HOME/repos/amedia/site-config
 alias sitetool="$HOME/repos/amedia/sitetool/bin/sitetool"
 
 abbr -a a a2l
+
+function deploy
+    if test (count $argv) -ne 3
+        echo "Usage: amedia-deploy <app> <env> <version>"
+        return 1
+    end
+    set app $argv[1]
+    set env $argv[2]
+    set ver $argv[3]
+    xdg-open "https://secure.amedia.cloud/api/idp-github-app/deploy/amedia/$app/$env/$app/$ver"
+end
