8bc6c161940f276bad521be7847b1c0e719cdd9f
Guðmundur Björn Birkisson <gbb@gbb.is>
2026-09-25 12:58:06 +0200

    fix: pick function open on line

--- .config/fish/conf.d/pick.fish
+++ .config/fish/conf.d/pick.fish
@@ -4,7 +4,7 @@ end
 
 function pick --description "pick path from stdin: cd into dirs, open files in nvim, print if piped"
     set -l entries
-    awk -F: '{ k = $1 ($2 ~ /^[0-9]+$/ ? ":" $2 : "") } !seen[k]++ { print k }' | while read -l entry
+    awk '!seen[$0]++' | while read -l entry
         test -e (string split -f1 : -- $entry); and set -a entries $entry
     end
     test (count $entries) -gt 0; or return 1
@@ -13,12 +13,18 @@ function pick --description "pick path from stdin: cd into dirs, open files in n
     or return
 
     set -l parts (string split -m1 : -- $pick)
+    set -l line
+    if set -q parts[2]
+        set line (string match -r '^\d+(?=:|$)' -- $parts[2])
+        or set line (grep -nF -m1 -- $parts[2] $parts[1] | string split -f1 :)
+    end
+
     if not isatty stdout
         echo $parts[1]
     else if test -d $parts[1]
         cd $parts[1]
-    else if set -q parts[2]
-        nvim +$parts[2] $parts[1] </dev/tty
+    else if test -n "$line"
+        nvim +$line $parts[1] </dev/tty
     else
         nvim $parts[1] </dev/tty
     end
